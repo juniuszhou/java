e3c757077dc6845f6c829551c66afd9af136565f
juniuszhou <junius.zhou@gmail.com>
2014-10-15 13:01:07 +0800

    moving folder

--- practice/src/com/company/Errors.java
+++
@@ -1,6 +0,0 @@
-
-class Errors{
-    public static void main(String [] args){
-     System.out.println();
-    }
-}
--- practice/src/com/company/exams.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by junijun on 7/17/14.
- */
-public class exams {
-    public static void main(String[] args){
-        System.out.println();
-    }
-}
