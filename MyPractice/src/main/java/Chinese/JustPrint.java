474b0ac848b99888f75244d8490d23c7af71a9f9
juniuszhou <junius.zhou@gmail.com>
2015-03-27 08:59:27 +0800

    merge modules

--- MyPractice/src/main/java/Chinese/JustPrint.java
+++ MyPractice/src/main/java/Chinese/JustPrint.java
@@ -1,9 +1,6 @@
 package Chinese;
 
 public class JustPrint {
-    private static double tolerance = 0.01;
-    public static double getTolerance() {return tolerance;}
-    public static void setTolerance(double d) {tolerance = d;}
     public static void main(String[] args) {
 
     }
