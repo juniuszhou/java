e21d15c9834be56410b3b4b0287ba66a546a233c
juniuszhou <junius.zhou@gmail.com>
2015-05-13 14:14:29 +0800

    put all under src

---
+++ src/main/java/MyPractice/Algo/JustTry.java
@@ -0,0 +1,10 @@
+package main.java.Algo;
+
+public class JustTry {
+
+    public static void main(String[] args){
+        String str = "101"
+                + "\n3";
+        System.out.println(str);
+    }
+}
---
+++ src/main/java/MyPractice/MyThread/MyRunnable.java
@@ -0,0 +1,30 @@
+package main.java.MyThread;
+
+class MyRun implements Runnable{
+    private int a = 100;
+    @Override
+    public void run(){
+        while(a > 0){
+            try {
+                a = a - 1;
+                System.out.println(Thread.currentThread().getName() + a);
+                Thread.sleep(1000);
+            }
+            catch (Exception e){
+
+            }
+        }
+    }
+
+}
+public class MyRunnable {
+
+    public static void main(String[] args){
+        MyRun r = new MyRun();
+        //Thread tt = new Thread(r);
+        for(int i = 0; i < 10; ++i){
+            Thread t = new Thread(r);
+            t.start();
+        }
+    }
+}
